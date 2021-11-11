c856cd61951160995f8e2376b7fc93c4493e8c71
Francisco Scott <franciscoscott@MacBook-Air-de-Francisco.local>
2021-11-11 17:27:24 -0300

    empezamos con el home.js

--- javascript/home.js
+++ javascript/home.js
@@ -1,6 +1,41 @@
-window.addEventListener("load", function(){
+window.onload = function() {
 
-/* PELÍCULAS POPULARES EN WeStream */
+const apiKey = "ce6192ca048843364a3b46473fb16087" ;
+
+//Formulario del header
+
+//Pagina Home de WeStream
+
+//Peliculas Populares en We Stream
+
+let url = (`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`)
+
+let peliculasPopulares = document.querySelector(".sectioncontenedora")
+
+fetch (url)
+
+.then (function(respuesta){
+    return respuesta.json();
+})
+.then(function(informacion){ 
+    for (let i = 0; i <= 5; i++){ 
+    console.log(informacion.results[i]);
+    
+    //Comenzamos con la estructura de nuestra pagina a partir de innerHTML
+
+        peliculasPopulares.innerHTML += `
 
 
-});
+           
+        `
+}
+})
+.catch(function (error) {
+    console.log('el error fue ' + error);
+})
+
+//Series Populares en We Stream
+
+//Lo más visto en Peliculas de WeStream
+
+}
