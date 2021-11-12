0274a75e53383199be931eb4ef377cf216306da2
Francisco Scott <franciscoscott@MacBook-Air-de-Francisco.local>
2021-11-12 18:31:48 -0300

    avances en js

--- javascript/home.js
+++ javascript/home.js
@@ -21,12 +21,16 @@ fetch (url)
     for (let i = 0; i <= 5; i++){ 
     console.log(informacion.results[i]);
     
-    //Comenzamos con la estructura de nuestra pagina a partir de innerHTML
+    //estructura de nuestra pagina a partir de innerHTML
 
         peliculasPopulares.innerHTML += `
+        <article>
+    <a href= "./javascript/detail-movie.js?id=${datos.results[i].id}" class= "titulos2"> ${datos.results[i].title} </a>
 
+    <div> <a href="./detail-movie.html?id=${datos.results[i].id}"> <img src="https://image.tmdb.org/t/p/w342/${datos.results[i].poster_path}" alt="${datos.results[i].title}" class="imagenesdetail"> </a> </div>
 
-           
+    <p>Fecha de Estreno: ${datos.results[i].release_date}</p>
+    </article>  
         `
 }
 })
